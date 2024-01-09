841b8c2a66518c4d474b1446b64cc5d9d51906d6
Ashcoder666 <ashirnbasheer@gmail.com>
2024-01-09 22:46:27 +0530

    structure is complete

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,11 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
+dotenv.config();
 
-dotenv.config()
-
+export const NODE_ENV = process.env.NODE_ENV || "development";
 export const PORT = process.env.PORT || 4000;
 export const DEVELOPMENT_URL = `http://localhost:${PORT}`;
 export const PRODUCTION_URL = process.env.PRODUCTION_URL;
+export const HOST_URL =
+  NODE_ENV === "development" ? DEVELOPMENT_URL : PRODUCTION_URL;
+export const SWAGGER_URL = `${HOST_URL}/api-docs/`;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
 import express, { Express } from "express";
-import cors from 'cors'
+import cors from "cors";
 import helmet from "helmet";
-import morgan from 'morgan'
-import cookieParse from 'cookie-parser'
-
+import morgan from "morgan";
+import cookieParse from "cookie-parser";
+import swaggerUI from "swagger-ui-express";
+import { swaggerSpecs } from "./config/swaggerConfig";
+import { PORT, HOST_URL, SWAGGER_URL } from "./constants";
 
 const app: Express = express();
-app.use(cors())
-app.use(helmet())
-app.use(morgan("dev"))
+app.use(cors());
+app.use(helmet());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParse());
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at ${HOST_URL}`);
+  console.log(`API Documentation: ${SWAGGER_URL}`);
+});
