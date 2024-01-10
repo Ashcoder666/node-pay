9121ba114b91aba2d7e51677078e895f56f292d8
Ashcoder666 <ashirnbasheer@gmail.com>
2024-01-10 22:07:46 +0530

    added services and controller

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -10,3 +10,22 @@ interface IUser extends Document {
     bank_name: string;
   };
 }
+
+interface IUserDB extends IUser, Document {
+  _id: Types.ObjectId;
+}
+
+const userSchema: Schema<IUserDB> = new Schema<IUserDB>({
+  full_name: { type: String, required: true },
+  phone_number: { type: Number, required: true },
+  email: { type: String, required: true },
+  bank_details: {
+    account_no: { type: Number, required: true },
+    ifsc: { type: String, required: true },
+    bank_name: { type: String, required: true },
+  },
+});
+
+const userModel = mongoose.model<IUserDB>("users", userSchema);
+
+export { userModel, IUserDB };
---
+++ src/services/userService.ts
@@ -0,0 +1,25 @@
+import { userModel } from "../models/userModel";
+
+class UserService {
+  userSignup = async (
+    full_name: string,
+    phone_number: number,
+    email: string,
+    bank_details: {
+      account_no: number;
+      ifsc: string;
+      bank_name: string;
+    }
+  ) => {
+    const newUser = await userModel.create({
+      full_name,
+      phone_number,
+      email,
+      bank_details,
+    });
+
+    return newUser;
+  };
+}
+
+export default UserService;
